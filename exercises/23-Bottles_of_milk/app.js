7cd3743610ac5c91e7ae7522145a4542c449b66d
kath424 <acostak424@gmail.com>
2024-03-08 10:36:02 +0000

    Ejercicios de Js - terminados.

--- exercises/23-Bottles_of_milk/app.js
+++ exercises/23-Bottles_of_milk/app.js
@@ -1,16 +1,20 @@
 // Your code here: 
 let string1 = " bottles of milk on the wall, "
-let string2 = " bottles of milk. Take one down and pass it around "
-let string3 = "No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall."
+let string2 = " bottles of milk. Take one down and pass it around, "
+let string3 = " bottles of milk on the wall."
+let string4 = " bottle of milk on the wall."
+let string5 = "No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall."
 
-console.log(window);
-
-for(let i = count; i >= 1 ; i--){
-    if(i !== 1){
-        console.log(i + string1 + i + string2)
+for(let i = 99; i >= 1 ; i--){
+    if(i > 2){
+        console.log(i + string1 + i + string2 + (i - 1) + string3);
+    }
+    else if(i == 2){
+        console.log(i + string1 + i + string2 + (i - 1) + string4);
+    
     }else {
-        console.log(i + " bottle of milk on the wall " + i + " Take one down and pass it around, no more bottles of milk on the wall.");
+        console.log(i + " bottle of milk on the wall, " + i + " bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.");
     }
 
 }
-console.log(string3);
+console.log(string5);
--- exercises/24-Javascript_Objects/app.js
+++ exercises/24-Javascript_Objects/app.js
@@ -18,7 +18,7 @@ var person2 = {
 
 var family = {
     lastName: "Doe",
-    members: [person, person2, person3]   //Array of objects, don't forget to add Jimmy
+    members: [person, person2]   //Array of objects, don't forget to add Jimmy
 };
 
 
@@ -27,13 +27,18 @@ function addAllFamilyLuckyNumbers(anArray){
 
   //To-Do: loop and add; consider nested loops
   //Hint: use the anArray variable to get all of the lucky numbers
+
+    for(let i = 0; i < anArray.length; i++){
+        for(let j = 0; j < anArray[i].luckyNumbers.length; j++){
+            sumOfAllLuckyNumbers += anArray[i].luckyNumbers[j];
+        }
+    }
   
   return sumOfAllLuckyNumbers;
 }
 
 //Enter all your code here:
 person.luckyNumbers[3] = 33;
-console.log(person)
 
 var person3 = {
     name: "Jimmy",
@@ -43,7 +48,7 @@ var person3 = {
     luckyNumbers: [1, 2, 3, 4],
     significantOther: null
 };
-
+family.members.push(person3);
 
 //Do not make changes below:
 console.log(addAllFamilyLuckyNumbers(family.members)); 
